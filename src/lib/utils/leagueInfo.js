42ddcffac75067b0d7efbcf90efa69de8aeff97d
ericolive23 <143529174+ericolive23@users.noreply.github.com>
2023-11-30 10:54:39 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -57,7 +57,7 @@ export const managers = [
       "favoritePlayer": 142, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "philosophy": "My team is like the United States government. Spend all the money, build back better, but with FAAB.", // (optional)
       "tradingScale": 8, // 1 - 10 (optional)
-      "preferredContact": "Phone",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
       "managerID": "991180287608500224",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
@@ -75,7 +75,7 @@ export const managers = [
       "favoritePlayer": 910, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "philosophy": "RBs first, second, and third", // (optional)
       "tradingScale": 1, // 1 - 10 (optional)
-      "preferredContact": "Phone",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
       "managerID": "732784610907365376",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
@@ -93,7 +93,7 @@ export const managers = [
       "favoritePlayer": 972, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "philosophy": "I seethe to find a way to devastate my fellow competitors, eventually failing in anguish", // (optional)
       "tradingScale": 7.22, // 1 - 10 (optional)
-      "preferredContact": "Phone",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
       "managerID": "991188749172191232",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
@@ -113,6 +113,60 @@ export const managers = [
       "tradingScale": 6.5, // 1 - 10 (optional)
       "preferredContact": "Carrier Pigeon",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
+  {
+      "managerID": "992483971810361344",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+      "name": "Paul Cassaro",
+      "location": "Washington Heights, NYC", // (optional)
+      "bio": "Paul joined the league in 2020, riding the prolific success of Dalvin Cook to a league best 12-3 record and falling just short of a championship in that season's playoff. Although it's been tough sledding since his rookie year, Paul's competitive edge has not wavered. You can usually find him making questionable waiver wire claims and head-scratching trades (that he swears will turn his season around). But give the man some credit - he'll do whatever it takes to win... and when he doesn't, at least the horrible transactions will make for a good laugh.",
+      "photo": "/managers/PaulC.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "rival": {
+        name: "Kevin Richardson", // Can be anything (usually your rival's name)
+        link: "5", // manager array number within this array, or null to link back to all managers page
+        image: "/managers/KevinR.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
+      "favoritePlayer": 3294, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "philosophy": "Trading is fun and better than pretending like you're some guru who might have picked a perfect team on draft night.", // (optional)
+      "tradingScale": 10, // 1 - 10 (optional)
+      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    },
+    {
+      "managerID": "990290994589425664",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+      "name": "Corey Smith",
+      "location": "The 'DUH state", // (optional)
+      "bio": "daddy doing daddy things beating my competition as hard as my kids",
+      "photo": "/managers/CoreyS.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "rival": {
+        name: "Me, since I only beat myself", // Can be anything (usually your rival's name)
+        link: "7", // manager array number within this array, or null to link back to all managers page
+        image: "/managers/CoreyS.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
+      "favoritePlayer": 1947, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "philosophy": "Now that I'm older, I realize this has been training me for being a dad you have no idea what's going to happen week after week or if the one you have is the 'good one' but you invest all your time into it and love it no matter what. Plus like kids, you can always just try again next year.", // (optional)
+      "tradingScale": 5.5, // 1 - 10 (optional)
+      "preferredContact": "Email",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    }
+    {
+      "managerID": "991181142994862080",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+      "name": "George Smith",
+      "location": "Summerville", // (optional)
+      "bio": "Wealth, fame, power. I obtained this and everything else ",
+      "photo": "/managers/CoreyS.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "rival": {
+        name: "since I only beat myself", // Can be anything (usually your rival's name)
+        link: "7", // manager array number within this array, or null to link back to all managers page
+        image: "/managers/CoreyS.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
+      "favoritePlayer": 1947, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "philosophy": "Now that I'm older, I realize this has been training me for being a dad you have no idea what's going to happen week after week or if the one you have is the 'good one' but you invest all your time into it and love it no matter what. Plus like kids, you can always just try again next year.", // (optional)
+      "tradingScale": 5.5, // 1 - 10 (optional)
+      "preferredContact": "Email",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    }
     ]
   
   
