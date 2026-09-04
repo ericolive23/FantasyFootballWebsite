bc9a76d7a6601695724b3eec9db2121c2c80f878
ericolive23 <143529174+ericolive23@users.noreply.github.com>
2026-09-04 17:12:54 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1245382005625999360"; // your league ID 
-export const leagueName = "Snitches get Stitches"; // your league name
+export const leagueID = "1376931957371318272"; // your league ID 
+export const leagueName = "Rob Patty Fan Club"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
